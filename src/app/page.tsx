826453280398a601f4a4dc663c3293a9970cff82
chrisrex007 <krishcharniya007@gmail.com>
2025-03-02 17:57:33 +0530

    route to login/register

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
-import Image from "next/image";
+// Welcome Screen
 
-export default function Home() {
-  return <></>;
-}
+"use client";
+import { useRouter } from "next/navigation";
+
+const Home = () => {
+  const router = useRouter();
+  return (
+    //outer div
+    <div className="flex">
+      {/* Login Button */}
+      <button
+        className="text-red-200 m-12 p-12"
+        onClick={() => {
+          // push and replace both methods are available, not yet decided what to choose
+          router.push("/login");
+        }}
+      >
+        Login
+      </button>
+
+      {/* Register Button */}
+      <button
+        className="text-blue-200 m-12 p-12 borderborder-amber-200"
+        onClick={() => {
+          // push and replace both methods are available, not yet decided what to choose
+          router.push("/register");
+        }}
+      >
+        Register
+      </button>
+    </div>
+  );
+};
+
+export default Home;
