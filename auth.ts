eadebb68224b0a3057c0f7e2536ad3da3aa7cafb
Dami-18 <damiyagondha@gmail.com>
2025-03-04 19:02:50 +0530

    configure nextauth

---
+++ auth.ts
@@ -0,0 +1,48 @@
+import NextAuth from "next-auth";
+import { ZodError } from "zod";
+import Credentials from "next-auth/providers/credentials";
+import { signInSchema } from "./src/lib/zod";
+import { saltAndHashPassword } from "./src/utils/password"; // to write own logic for hashing and salting, don't know this path giving error
+import { getUserFromDb } from "./src/utils/db";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        try {
+          let user = null;
+
+          const { email, password } = await signInSchema.parseAsync(
+            credentials
+          );
+
+          // logic to salt and hash password
+          const pwHash = saltAndHashPassword(credentials.password);
+
+          // logic to verify if the user exists, these logics are yet to be implemented
+          user = await getUserFromDb(credentials.email, pwHash);
+
+          if (!user) {
+            // No user found, so this is their first attempt to login
+            // Optionally, this is also the place you could do a user registration
+            throw new Error("Invalid credentials.");
+          }
+
+          // return user object with their profile data
+          return user;
+        } catch (error) {
+          if (error instanceof ZodError) {
+            // Return `null` to indicate that the credentials are invalid
+            return null;
+          }
+        }
+      },
+    }),
+  ],
+});
--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,1 @@
-import jwt from 'jsonwebtoken';
-import { Request, Response, NextFunction } from 'express';
-
-export default function requireAuth(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) {
-      return res.status(401).json({ message: 'Unauthorized' });
-    }
-    try {
-      jwt.verify(token, process.env.JWT_SECRET || 'your_secret_key'); // yet to handle the env variables
-      next();
-    } catch (err) {
-      res.status(401).json({ message: 'Unauthorized' });
-    }
-  }
+export { auth as middleware } from "@/auth"
