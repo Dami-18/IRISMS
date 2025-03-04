404339c3113a388eefc54b74a18003640ac25530
chrisrex007 <krishcharniya007@gmail.com>
2025-03-04 19:01:30 +0530

    future comments

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,17 +1,16 @@
 // Register Page
 
-import { ChevronDownIcon } from "@heroicons/react/16/solid";
+import Form from "next/form";
 
-export default function Example() {
+const Register = () => {
   return (
-    <form>
+    // Basically action will be a function which will be server sided call it signup.ts we have to validate the data first, using zod library(my suggestion but can do regex method from the scratch)
+    // in the same file if the user has entered all the data correctly then call the backend and create a new user so tbh isme bas ab registration info,design change hogi and action me function name aayega basssssss backend se bas we'll do one api call
+    <Form action="/todonai pata meko ki kya karna he abhi but figure out kr lenge vro">
       <div className="space-y-12 p-20">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base/7 font-semibold">Profile</h2>
-          <p className="mt-1 text-sm/6 text-gray-600">
-            This information will be displayed publicly so be careful what you
-            share.
-          </p>
+          <p className="mt-1 text-sm/6 text-gray-600"></p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-4">
@@ -20,9 +19,7 @@ export default function Example() {
               </label>
               <div className="mt-2">
                 <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                  <div className="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">
-                    workcation.com/
-                  </div>
+                  <div className="shrink-0 text-base text-gray-500 select-none sm:text-sm/6"></div>
                   <input
                     id="username"
                     name="username"
@@ -127,10 +124,6 @@ export default function Example() {
                   <option>Canada</option>
                   <option>Mexico</option>
                 </select>
-                <ChevronDownIcon
-                  aria-hidden="true"
-                  className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                />
               </div>
             </div>
 
@@ -214,6 +207,7 @@ export default function Example() {
           Save
         </button>
       </div>
-    </form>
+    </Form>
   );
-}
+};
+export default Register;
