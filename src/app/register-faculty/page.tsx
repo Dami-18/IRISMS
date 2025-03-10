dc6d91319fba965a1629ba2852a3963149026d07
Dami-18 <damiyagondha@gmail.com>
2025-03-10 17:41:26 +0530

    add db model for prof

--- src/app/register-faculty/page.tsx
+++ src/app/register-faculty/page.tsx
@@ -182,7 +182,7 @@ const Register = () => {
 
                         <div className="sm:col-span-3">
                             <label htmlFor="highestDegree" className="block text-sm/6 font-medium">
-                                Highest Degree
+                                Educational Qualification
                             </label>
                             <div className="mt-2 grid grid-cols-1">
                                 <select
@@ -277,11 +277,11 @@ const Register = () => {
                             <label htmlFor="researchInterns" className="block text-sm/6 font-medium">
                                 Are you looking for research interns?
                             </label>
-                            <div className="mt-2">
+                            <div className="mt-2 grid grid-cols-1">
                                 <select
                                     id="researchInterns"
                                     name="researchInterns"
-                                    className="block w-full rounded-md bg-white py-1.5 pl-3 pr-8 text-base outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 >
                                     <option value="yes">Yes</option>
                                     <option value="no">No</option>
