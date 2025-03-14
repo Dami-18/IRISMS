7c8c757dcab207e3f1c2f84428a148f6a12de6af
chrisrex007 <krishcharniya007@gmail.com>
2025-03-14 13:30:09 +0530

    validation

--- auth.ts
+++ auth.ts
@@ -1,20 +1,28 @@
 import {
-  SignupFormSchema,
+  signupStudentFormSchema,
+  signupProfFormSchema,
   FormState,
   SigninFormSchema,
 } from "@/lib/definitions";
 
-// import { PrismaClient } from "@prisma/client";
 import { hash, compare } from "bcrypt-ts";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { redirect } from "next/navigation";
 
 export async function signupStud(formState: FormState, formData: FormData) {
   // this signup function is only for students currently, we need to make another for profs
-  const validatedFields = SignupFormSchema.safeParse({
-    username: formData.get("username"),
+  const validatedFields = signupStudentFormSchema.safeParse({
     email: formData.get("email"),
     password: formData.get("password"),
+    firstName: formData.get("firstName"),
+    lastName: formData.get("lastName"),
+    contact: formData.get("contact"),
+    address: formData.get("address"),
+    city: formData.get("city"),
+    state: formData.get("state"),
+    gradYear: formData.get("gradYear"),
+    majors: formData.get("majors"),
+    insti: formData.get("insti"),
   });
 
   // If any form fields are invalid, return early
@@ -24,7 +32,7 @@ export async function signupStud(formState: FormState, formData: FormData) {
     };
   }
 
-  const { username, email, password } = validatedFields.data;
+  const { email, password } = validatedFields.data;
 
   const hashedPassword = await hash(password, 10);
   console.log("ok");
@@ -33,7 +41,6 @@ export async function signupStud(formState: FormState, formData: FormData) {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      username: username,
       email: email,
       password: hashedPassword,
     }),
@@ -73,9 +80,7 @@ export async function signupStud(formState: FormState, formData: FormData) {
 
 // function for register of prof
 export async function signupProf(formState: FormState, formData: FormData) {
-  // this signup function is only for students currently, we need to make another for profs
-  const validatedFields = SignupFormSchema.safeParse({
-    username: formData.get("username"),
+  const validatedFields = signupProfFormSchema.safeParse({
     email: formData.get("email"),
     password: formData.get("password"),
   });
@@ -87,28 +92,27 @@ export async function signupProf(formState: FormState, formData: FormData) {
     };
   }
 
-  const { username, email, password } = validatedFields.data;
+  const { email, password } = validatedFields.data;
 
   const hashedPassword = await hash(password, 10);
   // console.log("ok");
 
   const data = {
-      username: username,
-      email: email,
-      password: hashedPassword,
-      firstName: formData.get("first-name"),
-      lastName: formData.get("last-name"),
-      contact: formData.get("contact"),
-      website: formData.get("website"),
-      gscholar: formData.get("gscholar"),
-      qualification: formData.get("highestDegree"),
-      degreeYear: formData.get("gradYear"),
-      specialization: formData.get("specialization"),
-      institution: formData.get("institution"),
-      teachingExp: formData.get("teachingExp"),
-      researchExp: formData.get("researchExp"),
-      researchInterns: formData.get("researchInterns"),
-      cvUrl: formData.get("cv") // need to handle this separately maybe in another api call for file uploads
+    email: email,
+    password: hashedPassword,
+    firstName: formData.get("first-name"),
+    lastName: formData.get("last-name"),
+    contact: formData.get("contact"),
+    website: formData.get("website"),
+    gscholar: formData.get("gscholar"),
+    qualification: formData.get("highestDegree"),
+    degreeYear: formData.get("gradYear"),
+    specialization: formData.get("specialization"),
+    institution: formData.get("institution"),
+    teachingExp: formData.get("teachingExp"),
+    researchExp: formData.get("researchExp"),
+    researchInterns: formData.get("researchInterns"),
+    cvUrl: formData.get("cv"), // need to handle this separately maybe in another api call for file uploads
   };
 
   const res = await fetch("/api/createFaculty", {
@@ -176,7 +180,6 @@ export async function signin(formState: FormState, formData: FormData) {
     console.log("login successful");
     redirect("/dashboard-students"); // dynamic route created
     // here redirect after login, maybe store user id or username in cookies and then fetch his details on dashboard page
-    
   } else {
     console.log("invalid password!");
   }
