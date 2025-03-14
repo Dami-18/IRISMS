94bd53f847680049a9cf249324ef8c085ceae081
chrisrex007 <krishcharniya007@gmail.com>
2025-03-14 13:40:27 +0530

    removed username

--- src/app/old/page.tsx
+++ src/app/old/page.tsx
@@ -15,28 +15,6 @@ const Register = () => {
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-              <label htmlFor="username" className="block text-sm/6 font-medium">
-                Username
-              </label>
-              <div className="mt-2">
-                <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                  <div className="shrink-0 text-base text-gray-500 select-none sm:text-sm/6"></div>
-                  <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    defaultValue="ewfuwehuf"
-                    className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                  />
-                </div>
-              </div>
-            </div>
-            {/*change the error style create a modal or something for it*/}
-            {state?.errors?.username && (
-              <p className="text-red-800">{state.errors.username}</p>
-            )}
-
-            <div className="sm:col-span-3">
               <label htmlFor="password" className="block text-sm/6 font-medium">
                 Password
               </label>
