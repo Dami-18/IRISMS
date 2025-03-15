becb66d82f4ebbfb52921e1fa50c6453b798cdd8
chrisrex007 <krishcharniya007@gmail.com>
2025-03-15 13:14:42 +0530

    uniqueEmail checked

--- src/app/api/createStudent/route.ts
+++ src/app/api/createStudent/route.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
+import Redis from "ioredis";
 import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
+const redis = new Redis(6379, "localhost");
 
 export async function POST(req: NextRequest) {
   const { email, password } = await req.json();
@@ -23,10 +25,22 @@ export async function POST(req: NextRequest) {
 
   try {
     // ERROR SOLVED BY KRISH: npx prisma generate (if you're worried about the red underline error on the "data")
+    // npx prisma migrate dev --name init
+    const existingUser = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "Email already exists" },
+        { status: 400 }
+      );
+    }
     const result = await prisma.user.create({
       // giving error in parsing connection string
       data: { email, password, uid: studUid },
     });
+    await redis.del(email);
     return NextResponse.json(result);
   } catch (error) {
     console.error(error);
