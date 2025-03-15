a0aaf8baed28721318d1f7da54a8c1c60da815e9
Dami-18 <damiyagondha@gmail.com>
2025-03-15 16:13:40 +0530

    add api for get prof from db

---
+++ src/app/api/getProfFromDb/route.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+  const { email } = await req.json();
+  console.log(email);
+
+  const user = await prisma.prof.findUnique({
+    where: {
+      email: email,
+    },
+    select: {
+      id: true,
+      uid: true,
+      email: true,
+      password: true,
+    },
+  });
+
+  return NextResponse.json({
+    id: user?.id,
+    uid: user?.uid,
+    hashedPass: user?.password,
+    message: "user found",
+  });
+}
