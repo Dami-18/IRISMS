5e6dd717227d93a873dbc841c51ac66996fc84b5
chrisrex007 <krishcharniya007@gmail.com>
2025-03-15 13:13:48 +0530

    defval changed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -43,7 +43,7 @@ const Login = () => {
                   // type="email"
                   required
                   autoComplete="email"
-                  defaultValue="feijfiwj2ga@gkakcp.com"
+                  defaultValue="okthisisit@kgpian.iitkgp.ac.in"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -73,7 +73,7 @@ const Login = () => {
                   type="password"
                   required
                   autoComplete="current-password"
-                  defaultValue="gwhguhr"
+                  defaultValue="hello123@"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
