499b7286a6cb80ebd0039e2ec19ef3497d12b11d
chrisrex007 <krishcharniya007@gmail.com>
2025-03-18 19:49:35 +0530

    profile updated

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,39 @@
 // profile page
 // logic - in the stored token after login, check the uid and check if it is prof or student and display the profile details accordingly
 "use client";
-import { verifyToken } from "@/../auth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Profile = () => {
-  const [isProf, setIsProf] = useState(false);
+  const [isProf, setIsProf] = useState<boolean | undefined>(undefined);
+  useEffect(() => {
+    const fetchDetails = async () => {
+      const res = await fetch("api/isitstudorprof", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
 
-  return <></>;
+      const { student } = await res.json();
+
+      if (student === true) {
+        setIsProf(false);
+      } else if (student === false) {
+        setIsProf(true);
+      }
+    };
+
+    fetchDetails();
+  }, []);
+
+  if (isProf === undefined) {
+    return <>YOU'RE NOT ALLOWED</>;
+  } else if (isProf === true) {
+    return <>You're Prof</>;
+  } else if (isProf === false) {
+    return <>You're Student</>;
+  }
 };
 export default Profile;
 // this needs to be changed and display editable details of student or prof
