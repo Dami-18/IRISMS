bc6fec2c8f11783309b90d37b237e1497f313529
Dami-18 <damiyagondha@gmail.com>
2025-03-31 01:43:15 +0530

    resolve conflict

--- src/app/particular-internship-faculty/[slug]/page.tsx
+++ src/app/particular-internship-faculty/[slug]/page.tsx
@@ -150,13 +150,15 @@ const InternshipApplications = () => {
         <div className="bg-white rounded-lg shadow-md p-8 max-w-4xl mx-auto relative">
           <h1 className="text-3xl font-bold text-indigo-700 mb-6">
             Applications for {project?.name}
-          </h1>{" "}
+          </h1>
+
           <button
-            onClick={() => setShowModal(true)}
-            className="fixed bottom-7 right-6 mr-20 bg-indigo-600 text-white px-4 py-2 rounded-full shadow-lg hover:bg-indigo-700"
-          >
-            Schedule Interview
-          </button>
+          onClick={() => setShowModal(true)}
+          className="fixed bottom-6 left-6 bg-blue-600 text-white px-4 py-2 rounded-full shadow-lg hover:bg-blue-700"
+        >
+          Schedule Interview
+        </button>
+
           {/* Delete Project Button */}
           <div className="flex justify-end mb-6">
             <button
